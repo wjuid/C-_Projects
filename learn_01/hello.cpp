0d091b0e9a02717afc907a54e2933112e254ae7a
wjuid <wjuid@163.com>
2019-10-11 14:47:53 +0800

    change

--- learn_01/hello.cpp
+++ learn_01/hello.cpp
@@ -4,7 +4,5 @@ using namespace std;
 int main()
 {
     cout << "hello world is wjuid" <<endl;
-     cout << "hello world is wjuid" <<endl;
-      cout << "hello world is wjuid" <<endl;
     return 0;
 }
