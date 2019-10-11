e2a58d7a33ca7db9fea9530c111fe4495de481ec
wjuid <wjuid@163.com>
2019-10-11 14:45:02 +0800

    first commit

---
+++ learn_01/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "hello world is wjuid" <<endl;
+     cout << "hello world is wjuid" <<endl;
+      cout << "hello world is wjuid" <<endl;
+    return 0;
+}
